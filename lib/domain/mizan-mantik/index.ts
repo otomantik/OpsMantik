25d2272558879118089a5be0f5179456b44614c7
otomantik <mscopur@otomantik.com>
2026-05-06 21:51:04 +0300

    fix(oci): remove broken causal-dna exports from domain index

--- lib/domain/mizan-mantik/index.ts
+++ lib/domain/mizan-mantik/index.ts
@@ -6,4 +6,3 @@ export { evaluateAndRouteSignal } from './orchestrator';
 export type { PipelineStage, SignalPayload, EvaluateResult } from './types';
 export { OPSMANTIK_CONVERSION_NAMES } from './conversion-names';
 export { getEntropyScore, type EntropyResult } from './entropy-service';
-export { createCausalDna, appendBranch, toJsonb, buildMinimalCausalDna } from './causal-dna';
